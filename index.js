f2609bfa0cd9e93230b8d298acdf0dbb37d4430d
HiramOrd <hiram8012@gmail.com>
2021-08-30 13:19:58 -0500

    New Design

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // HTML
 const dino = document.getElementsByClassName('dino-sprite')[0];
 const playButton = document.getElementsByClassName('ui-button play')[0];
+const ground = document.getElementsByClassName('ground')[0];
 const feedButton = document.getElementsByClassName('ui-button feed')[0];
 const scoreLabel = document.getElementsByClassName('score')[0];
 const higthScoreLabel = document.getElementsByClassName('h-score')[0];
@@ -12,6 +13,7 @@ let playing = false;
 let duck = false;
 let spritesCounter = 1;
 let score = 0;
+let speed = 5;
 let hScore = (localStorage.getItem('hScore')) ? localStorage.getItem('hScore') : 0 ;
 
 
@@ -23,15 +25,16 @@ const keyDown = async({keyCode}) => {
 
     if(playing === false && (keyCode === 32 || keyCode === 38)) {
         playing = true;
+        ground.setAttribute('style', `animation: ground-move ${speed}s 0.6s infinite linear both;`);
         playButton.classList.remove('floating');
         playButton.classList.add('close');
         feedButton.classList.add('close');
     }
 
     if ((keyCode === 32 || keyCode === 38) && !dino.className.includes('jump')) {
-        dino['src'] = './data/dinoJ.png';
+        dino['src'] = './data/dino.png';
         dino.classList.add('jump');
-        await sleep(400);
+        await sleep(500);
         if(dino.className.includes('jump')) {
             dino.classList.remove('jump');
             dino['src'] = './data/dino.png';
@@ -56,11 +59,12 @@ higthScoreLabel.textContent = hScore.toString().padStart(5,"0");
 setInterval(() => {
     spritesCounter = (spritesCounter === 1) ? 2 : 1;
 
-    if( playing) {
+    if(playing) {
         
         score++;
         const actualScore = score.toString().padStart(5,"0");
         scoreLabel.textContent = actualScore;
+
         if(hScore < score)
             higthScoreLabel.textContent = actualScore;
 
@@ -73,6 +77,13 @@ setInterval(() => {
     }      
 }, 100);
 
+setInterval(() => {
+    if(speed>1.5 && playing) {
+        speed -= 0.4;
+        ground.setAttribute('style', `animation: ground-move ${speed}s infinite linear both;`);
+    }
+}, 10000);
+
 setTimeout(() => {
     playButton.classList.add('floating');
     gameUI.classList.add('scale-in-center');
@@ -81,5 +92,6 @@ setTimeout(() => {
 
 // Events
 window.addEventListener("keydown", keyDown);
+gameUI.addEventListener("click", () => keyDown({keyCode: 32}));
 window.addEventListener("keyup", keyUp);
 playButton.addEventListener("click", () => {keyDown({keyCode: 32})});
